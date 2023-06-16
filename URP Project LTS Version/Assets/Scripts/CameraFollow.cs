7a8e7e5c4d2cc9fac3e5e0df621c3cdf97478f4a
Atakan Kutay Soydan <soydan810@gmail.com>
2023-06-16 22:37:36 +0300

    Object Visibility
    obje alpha değeri değişimi scripti yapıldı

--- URP Project LTS Version/Assets/Scripts/CameraFollow.cs
+++ URP Project LTS Version/Assets/Scripts/CameraFollow.cs
@@ -9,9 +9,60 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private float smoothTime;
     private Vector3 _currentVelocity = Vector3.zero;
 
+
+    // Object Visibility
+    private ObjectVisibility _fader;
+    private GameObject player;
+
     private void Awake()
     {
         _offset = transform.position - target.position;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+
+    private void Update()
+    {
+
+        if (player != null)
+        {
+            Vector3 _direction = player.transform.position - transform.position;
+            Ray ray = new Ray(transform.position, _direction);
+            RaycastHit hit;
+
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.LogWarning("1. if içerisine girildi...");
+                if (hit.collider == null)
+                {
+                    Debug.LogWarning("2. if içerisine girildi...");
+                    return;
+                }
+                if (hit.collider.gameObject == player)
+                {
+                    Debug.LogWarning("3. if içerisine girildi...");
+
+                    if (_fader != null)
+                    {
+                        Debug.Log("Fade iþlemi yapýlamadý !");
+                        _fader.doFade = false;
+                    }
+                    else
+                    {
+                        Debug.LogWarning("else içerisine girdi...");
+                        _fader = hit.collider.gameObject.GetComponent<ObjectVisibility>();
+
+                        if (_fader != null)
+                        {
+                            Debug.Log("Fade Ýþlemi Yapýldý !");
+                            _fader.doFade = true;
+                        }
+                    }
+                  
+                }
+            }
+        }
     }
 
     private void LateUpdate()
--- URP Project LTS Version/Assets/Scripts/ObjectVisibility.cs
+++ URP Project LTS Version/Assets/Scripts/ObjectVisibility.cs
@@ -1,9 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectVisibility : MonoBehaviour
 {
+    public float fadeSpeed, fadeAmount;
+    private float originalOpacitiy;
+    Material[] _materials;
+    public bool doFade = false;
 
+    private void Start()
+    {
+        _materials = GetComponent<Renderer>().materials;
+
+        foreach (Material material in _materials)
+        {
+            originalOpacitiy = material.color.a;
+        }
+      
+    }
+
+    private void Update()
+    {
+        if (doFade)
+        {
+            FadeNow();
+        }
+        else
+        {
+            ResetFade();
+        }
+    }
+
+    private void FadeNow()
+    {
+        foreach (Material material in _materials)
+        {
+            Color currenColor = material.color;
+            Color smoothColor = new Color(currenColor.r, currenColor.g, currenColor.b, Mathf.Lerp(currenColor.a, fadeAmount, fadeSpeed * Time.deltaTime));
+            material.color = smoothColor;
+        }
+    }
+
+    private void ResetFade()
+    {
+        foreach (Material material in _materials)
+        {
+            Color currenColor = material.color;
+            Color smoothColor = new Color(currenColor.r, currenColor.g, currenColor.b, Mathf.Lerp(currenColor.a, originalOpacitiy, fadeSpeed * Time.deltaTime));
+            material.color = smoothColor;
+        }
+    }
 }
 
